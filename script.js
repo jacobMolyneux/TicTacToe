ed2fc6589862921d4ed892a2d4817df8de7a3ed0
Jacob Molyneux <79246480+jacobMolyneux@users.noreply.github.com>
2021-06-21 06:01:39 -0400

    fixed a problem with the player icons being displayed on click

--- script.js
+++ script.js
@@ -1,47 +1,58 @@
-
-
-const player = (name, icon) =>{
-    const sayHello = () => console.log(`Hello ${player.name}`);
-    return {sayHello, name, icon}
-}
+const player = (name, icon) => {
+  const sayHello = () => console.log(`Hello ${player.name}`);
+  return { sayHello, name, icon };
+};
 let i = 0;
+
 const gameBoard = (player1, player2) => {
-    const gameDisplay = document.getElementById('container');
-    const startGame = () => {
-        console.log("start game has started! But the loop has not started yet");
-        for(let i = 0; i < 9; i++){
-            console.log("the loop has started!");
-            const gameDisplay = document.getElementById('container');
-            console.log("container has been retrieved");
-            const box = document.createElement('div');
-            console.log("the box has been created!");
-            box.classList = 'box';
-            console.log("the class list has been created");
-            box.addEventListener('click', function(){
-                
-                console.log("the event listener is being added.")
-                
-                if(i%2 == 0){
-                box.textContent = player1.icon;
-                i++;
-                }
-                
-                else{
-                    box.textContent = player2.icon;
-                    i++;
-                }
-                console.log(`the value of i is: ${i}`);
-                
-            });
-            gameDisplay.appendChild(box);
-            console.log("box is being appended!");
-        };
-    };
-    return {gameDisplay, startGame};
-    
-}
-player1 = player('jacob', "X");
-player2 = player('bob', "O");
+  const gameDisplay = document.getElementById("container");
+
+  const startGame = () => {
+    let clickCounter = 0;
+    for (let i = 0; i < 9; i++) {
+      const gameDisplay = document.getElementById("container");
+      const box = document.createElement("div");
+      box.classList = "box";
+      box.addEventListener("click", function () {
+        console.log("the event listener is being added.");
+        if (clickCounter % 2 == 0) {
+          box.textContent = player1.icon;
+          clickCounter++;
+        } else {
+          box.textContent = player2.icon;
+          clickCounter++;
+        }
+      });
+      gameDisplay.appendChild(box);
+    }
+    const Player1NameDiv = document.getElementById("player1Name");
+    Player1NameDiv.textContent = player1.name;
+    const Player1IconDiv = document.getElementById("player1Icon");
+    Player1IconDiv.textContent = player1.icon;
+
+    const Player2NameDiv = document.getElementById("player2Name");
+    Player2NameDiv.textContent = player2.name;
+    const Player2IconDiv = document.getElementById("player2Icon");
+    Player2IconDiv.textContent = player2.icon;
+  };
+  return { gameDisplay, startGame };
+};
+
+// get player info
+player1Name = document.getElementById("player1Name").value;
+player2Name = document.getElementById("player2Name").value;
+player1 = player(player1Name, "X");
+player2 = player(player2Name, "O");
 NewGame = gameBoard(player1, player2);
 NewGame.startGame();
 
+winnerCheck = (gameArray) => {
+  winningArrays = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+  ];
+};
